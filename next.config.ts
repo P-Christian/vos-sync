5103eeb487200a0fcdf0618e9579c45dbe7cc423
Maki <Fierrez@users.noreply.github.com>
2026-08-01 09:00:39 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins:['vos-sync-local','vertex-dev']
+  allowedDevOrigins:['vos-sync-local','vertex-dev','100.114.249.96']
 };
 
 export default nextConfig;
